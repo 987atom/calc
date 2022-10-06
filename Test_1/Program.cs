e6a584a9a26e2c51fd23d83f79de56a68482ae96
987atom <isip_m.k.davydov@mpt.ru>
2022-10-06 09:53:55 +0300

    изминил проектт

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            int c = 0;
+            while (c != 9)
+            {
             Console.WriteLine("Введите первое число");
             string a = Console.ReadLine();
             float x = Convert.ToInt32(a);
@@ -20,92 +23,83 @@
                 " 7. Найти 1 процент от числа" +
                 " 8. Найти факториал из числа" +
                 " 9. Выйти из программы");
-            int z = 1;
             string c1 = Console.ReadLine();
-            int c = Convert.ToInt32(c1);
-            while (z == 1)
-            {
-                while (c != 9)
+             c = Convert.ToInt32(c1);
+                if (c == 1)
                 {
-                    if (c == 1)
-                    {
-                        Console.WriteLine("Ответ:");
-                        Console.WriteLine(x + y);
-                        break;
-                    }
-                    if (c == 2)
-                    {
-                        Console.WriteLine("Ответ:");
-                        Console.WriteLine(x - y);
-                        break;
-                    }
-                    if (c == 3)
-                    {
-                        Console.WriteLine("Ответ:");
-                        Console.WriteLine(x * y);
-                        break;
-                    }
-                    if (c == 4)
-                    {
-                        Console.WriteLine("Ответ:");
-                        Console.WriteLine(x / y);
-                        break;
-                    }
-                    if (c == 5)
-                    {
-                        float i = x;
-                        while (i != Math.Pow(x, y))
-                        {
-                            i = i * y;
-                        }
-                        Console.WriteLine("Ответ:");
-                        Console.WriteLine(i);
-                        break;
-                    }
-                    if (c == 6)
-                    {
-                        Console.WriteLine("Корень первого числа");
-                        Console.WriteLine(Math.Sqrt(x));
-                        Console.WriteLine("Корень второго числа");
-                        Console.WriteLine(Math.Sqrt(y));
-                        break;
-                    }
-                    if (c == 7)
-                    {
-                        float i1 = x;
-                        i1 = i1 / 100;
-                        Console.WriteLine("1% от первого числа равен");
-                        Console.WriteLine(i1);
-                        y = y / 100;
-                        Console.WriteLine("1% от второго числа равен");
-                        Console.WriteLine(y);
-                        break;
-                    }
-                    if (c == 8)
-                    {
-                        float n = 1;
-                        float fact = 1;
-                        while (n < x)
-                        {
-                            n = n + 1;
-                            fact = fact * n;
-                        }
-                        Console.WriteLine("Факториал первого числа равен");
-                        Console.WriteLine(fact);
-                        float t = 1;
-                        float fa = 1;
-                        while (t < y)
-                        {
-                            t = t + 1;
-                            fa = fa * t;
-                        }
-                        Console.WriteLine("Факториал второго числа равен");
-                        Console.WriteLine(fa);
-                        break;
-                    }
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(x + y);
                 }
-                Console.WriteLine("Программа завершена");
-                break;
+                if (c == 2)
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(x - y);
+                }
+                if (c == 3)
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(x * y);
+                }
+                if (c == 4)
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(x / y);
+                }
+                if (c == 5)
+                {
+                    float i = x;
+                    do
+                    {
+                        i = i * y;
+                    }
+                    while (i != Math.Pow(x, y));
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(i);
+                }
+                if (c == 6)
+                {
+                    Console.WriteLine("Корень первого числа");
+                    Console.WriteLine(Math.Sqrt(x));
+                    Console.WriteLine("Корень второго числа");
+                    Console.WriteLine(Math.Sqrt(y));
+                }
+                if (c == 7)
+                {
+                    float i1 = x;
+                    i1 = i1 / 100;
+                    Console.WriteLine("1% от первого числа равен");
+                    Console.WriteLine(i1);
+                    y = y / 100;
+                    Console.WriteLine("1% от второго числа равен");
+                    Console.WriteLine(y);
+                }
+                if (c == 8)
+                {
+                    float n = 1;
+                    float fact = 1;
+                    while (n < x)
+                    {
+                        n = n + 1;
+                        fact = fact * n;
+                    }
+                    Console.WriteLine("Факториал первого числа равен");
+                    Console.WriteLine(fact);
+                    float t = 1;
+                    float fa = 1;
+                    while (t < y)
+                    {
+                        t = t + 1;
+                        fa = fa * t;
+                    }
+                    Console.WriteLine("Факториал второго числа равен");
+                    Console.WriteLine(fa);
+                }
+                if (c == 9)
+                {
+                    break;
+                }
+                c1 = Console.ReadLine();
+                c = Convert.ToInt32(c1);
             }
             //a это строка
             //b это строка
